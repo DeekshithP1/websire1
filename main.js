9b525616bffdbc96bc3f7974d1aacaa1550e8772
DeekshithP1 <77963883+DeekshithP1@users.noreply.github.com>
2021-01-25 15:45:19 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+function toggle() {
+  let menu = document.getElementById("side-menu");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  }
+  else {
+    menu.style.display = "block";
+  }
+}
+
+
+
